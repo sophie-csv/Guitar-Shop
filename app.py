cdf64a1d51e1a31345e823b5922553e02e1a8bf5
216473 <sophieecastellon@gmail.com>
2024-11-06 11:11:00 -0500

    14:51

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from model import guitar_shop_DAL as db
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
+
+db.set_db()
