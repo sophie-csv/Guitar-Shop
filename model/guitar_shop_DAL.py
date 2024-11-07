cc9e1752c911e44db5117517490b5e61c839bbb9
216473 <sophieecastellon@gmail.com>
2024-11-07 10:46:29 -0500

    Comments completed

--- model/guitar_shop_DAL.py
+++ model/guitar_shop_DAL.py
@@ -1,6 +1,8 @@
 import json
 
-
+"""
+Sets up the initial database.
+"""
 def set_db():
     init_db = {
         "Guitars": {
@@ -46,35 +48,54 @@ def set_db():
     json.dump(init_db, f)
     f.close()
 
+"""
+Loads the database from a json file and returns it as a dict to use in other methods.
+"""
 def get_db_as_dict():
     f = open('database/guitar_shop_DB.json', 'r')
     data = json.load(f)
     f.close()
     return data
 
+"""
+Writes to the database and adds it to the file. 
+"""
 def write_to_db(data):
     f = open('database/guitar_shop_DB.json', 'w')
     json.dump(data, f)
     f.close()
 
+"""
+Adds a category to the database and adds it to the file.
+Category: Category to add
+"""
 def add_category(category):
     data = get_db_as_dict()
     data[category] = {}
     write_to_db(data)
 
+"""
+Deletes a category to the database and remove it to the file.
+Category: Category to delete
+"""
 def delete_category(category):
     data = get_db_as_dict()
     del data[category]
     write_to_db(data)
 
-def get_category(category):
-    data = get_db_as_dict()
-    return data[category]
-
+"""
+Returns all categories as a list
+"""
 def get_all_categories_as_list():
     data = get_db_as_dict()
     return list(data)
-
+"""
+Adds a product to the database and adds it to the file.
+Category: Category to add
+Code: Code to identify
+Name: Name to identify
+Price: Price to identify
+"""
 def add_product(category,code,name,price):
     data = get_db_as_dict()
     data[category][code] = {
@@ -83,20 +104,39 @@ def add_product(category,code,name,price):
     }
     write_to_db(data)
 
+"""
+Deletes a product to the database and removes it to the file.
+Category: Category to add
+Code: Code to identify
+Name: Name to identify
+Price: Price to identify
+"""
 def delete_product(category,code):
     data = get_db_as_dict()
     del data[category][code]
     write_to_db(data)
 
-#has subpoints, multiple keys
+"""
+Gets all categories from a specified category and returns it.
+Category: Category to get
+"""
 def get_all_products_in_category(category):
     data = get_db_as_dict()
     return data[category]
-
+"""
+Gets a product name from a specified category and code and returns it.
+Category: Category to get
+Code: Code to identify
+"""
 def get_product_name(category, code):
     data = get_db_as_dict()
     return data[category][code]
 
+"""
+Get a product price from a specified category and code and returns it.
+Category: Category to get
+Code: Code to identify
+"""
 def get_product_price(category, code):
     data = get_db_as_dict()
     return data[category][code]['price']
